29fc7d4efda2508b9f7e9d4ce6db8ebe2bb9881c
Prince Mehra <princemehra9024@gamil.com>
2026-06-25 10:33:01 +0530

    Fix HasPushSubscriptions fatal error

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use NotificationChannels\WebPush\HasPushSubscriptions;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasPushSubscriptions;
+    use HasFactory, Notifiable;
 
     protected $fillable = ['name', 'email', 'password', 'role', 'semester', 'branch', 'course', 'bio', 'xp', 'last_login_at'];
 
